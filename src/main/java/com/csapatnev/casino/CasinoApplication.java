cc09bd435a23ce028879c9733df360b76bc3a479
Inckrisz <inckrisz28@gmail.com>
2024-11-02 10:43:14 +0100

    http://localhost:8082 hibernate

--- src/main/java/com/csapatnev/casino/CasinoApplication.java
+++ src/main/java/com/csapatnev/casino/CasinoApplication.java
@@ -1,20 +1,23 @@
 package com.csapatnev.casino;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.springframework.boot.SpringApplication;
+import com.csapatnev.casino.JavaFXApplication;
+import org.h2.tools.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import javafx.scene.Parent;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
-@EnableScheduling
 public class CasinoApplication {
 	public static void main(String[] args) {
+		startH2ConsoleServer(); // Start H2 console server manually
 		JavaFXApplication.main(args);
 	}
-}
 
+	private static void startH2ConsoleServer() {
+		try {
+			Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}

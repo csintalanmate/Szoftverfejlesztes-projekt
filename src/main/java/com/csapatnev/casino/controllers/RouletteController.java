9efccf748c9225e3eb630c3336b5262a62a71b84
gergoszarka <szarkagergo01@gmail.com>
2024-11-08 00:21:02 +0100

    Roulette kerék számokkal golyóval Ellentétes forgás Pontos eredmény

--- src/main/java/com/csapatnev/casino/controllers/RouletteController.java
+++ src/main/java/com/csapatnev/casino/controllers/RouletteController.java
@@ -9,6 +9,8 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -20,7 +22,13 @@ import java.util.Random;
 public class RouletteController {
 
     @FXML
-    private Group polygonGroup;
+    private Group polygonGroupBigRoulette;
+
+    @FXML
+    private Group ball;
+
+    @FXML
+    private Polygon numberZeroPanel;
 
     @FXML
     private Button btnSwitchToMain;
@@ -28,17 +36,20 @@ public class RouletteController {
     @FXML
     private Text rollText;
 
+    public boolean pause = false;
+
     @FXML
     public void initialize() {
         // Set up the RotateTransition
+        /*
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(20), polygonGroup);
 
-        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(200), polygonGroup);
-
-        rotateTransition.setByAngle(3600); // Spin 360 degrees
+        rotateTransition.setByAngle(360); // Spin 360 degrees
         rotateTransition.setCycleCount(RotateTransition.INDEFINITE);
 
         rotateTransition.setAutoReverse(false); // No reverse
         rotateTransition.play(); // Start the animation
+        */
     }
 
     @FXML
@@ -50,20 +61,48 @@ public class RouletteController {
         stage.setScene(new Scene(signUpRoot));
     }
 
+    int startNumber = 0;
+    int[] order = {0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26};
+
+
     public void Roll()
     {
-        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(5), polygonGroup);
-        rotateTransition.setByAngle(360); // Spin 360 degrees
-        rotateTransition.setCycleCount(1);
-        rotateTransition.setAutoReverse(false); // No reverse
 
-        Random random = new Random();
-        rotateTransition.setOnFinished(event -> {
-            int randomNumber = getRandomNumber();
-            rollText.setText(String.valueOf(randomNumber));
+        int randomNumber = getRandomNumber();
+
+
+
+        double turnWheel = randomNumber * 9.7297297297 + 360;
+
+
+        RotateTransition rotateTransitionWheel = new RotateTransition(Duration.seconds(5), polygonGroupBigRoulette);
+        rotateTransitionWheel.setByAngle(-turnWheel); // Spin 360 degrees
+        rotateTransitionWheel.setCycleCount(1);
+        rotateTransitionWheel.setAutoReverse(false); // No reverse
+
+        int finalRandomNumber = randomNumber + startNumber;
+        startNumber = finalRandomNumber;
+        rotateTransitionWheel.setOnFinished(event -> {
+
+            rollText.setText(String.valueOf(order[(finalRandomNumber%37)*2]));
+
         });
 
-        rotateTransition.play();
+        RotateTransition rotateTransitionBall = new RotateTransition(Duration.seconds(5), ball);
+        rotateTransitionBall.setByAngle(turnWheel); // Spin 360 degrees
+        rotateTransitionBall.setCycleCount(1);
+        rotateTransitionBall.setAutoReverse(true);
+
+
+        rotateTransitionBall.setOnFinished(event -> {
+            numberZeroPanel.setFill(Color.YELLOW);
+        });
+
+
+
+        rotateTransitionBall.play();
+        rotateTransitionWheel.play();
+
     }
 
     private int getRandomNumber() {

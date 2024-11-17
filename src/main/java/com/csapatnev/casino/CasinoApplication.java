23d526bd21b69950f4bf6049d7ae40bb85e7fdeb
Inckrisz <inckrisz28@gmail.com>
2024-11-17 15:44:46 +0100

    8082 port bug fix

--- src/main/java/com/csapatnev/casino/CasinoApplication.java
+++ src/main/java/com/csapatnev/casino/CasinoApplication.java
@@ -8,14 +8,24 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class CasinoApplication {
+	private static Server h2Server; // Store reference to the H2 server
+
 	public static void main(String[] args) {
 		startH2ConsoleServer(); // Start H2 console server manually
-		JavaFXApplication.main(args);
+		JavaFXApplication.main(args); // Launch JavaFX application
+	}
+
+	public static void stopH2ConsoleServer() {
+		if (h2Server != null && h2Server.isRunning(false)) {
+			System.out.println("Stopping H2 console server...");
+			h2Server.stop();
+		}
 	}
 
 	private static void startH2ConsoleServer() {
 		try {
-			Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+			h2Server = Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+			System.out.println("H2 console server started on port 8082");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/csapatnev/casino/JavaFXApplication.java
+++ src/main/java/com/csapatnev/casino/JavaFXApplication.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class JavaFXApplication extends Application {
@@ -20,14 +19,17 @@ public class JavaFXApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
-        loader.setControllerFactory(springContext::getBean);
+        loader.setControllerFactory(springContext::getBean); // Use Spring to inject controllers
         primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.setTitle("Casino Application");
         primaryStage.show();
     }
 
     @Override
     public void stop() {
-        springContext.close();
+        System.out.println("Closing application...");
+        springContext.close(); // Close Spring context
+        CasinoApplication.stopH2ConsoleServer(); // Stop the H2 server
     }
 
     public static void main(String[] args) {

15c22d2b22fc19fdbd8281697ca9b23699184e7d
Inckrisz <inckrisz28@gmail.com>
2024-11-26 07:41:30 +0100

    admin controller

--- src/main/java/com/csapatnev/casino/controllers/AdminController.java
+++ src/main/java/com/csapatnev/casino/controllers/AdminController.java
@@ -1,17 +1,31 @@
 package com.csapatnev.casino.controllers;
 
+import com.csapatnev.casino.models.User;
+import com.csapatnev.casino.services.UserService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 @Controller
 public class AdminController implements Initializable {
+
+    @Autowired
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
 
     @FXML
     private Button btnCreate;
@@ -47,10 +61,28 @@ public class AdminController implements Initializable {
     private TableColumn<?, ?> columnRole;
 
     @FXML
-    private TableView<?> userTable;
+    private TableView<User> userTable;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<User> users = userService.findAll();  // Adjust this method as per your implementation
 
+        // Convert the list of users to an ObservableList
+        ObservableList<User> userObservableList = FXCollections.observableArrayList(users);
+
+        // Set the user list to the table
+        userTable.setItems(userObservableList);
+
+        // Set the cell value factories (mapping each column to the respective field in the User object)
+        columnId.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
+        columnFirstName.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        columnLastName.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        columnEmail.setCellValueFactory(cellData -> cellData.getValue().emailProperty());
+        columnDob.setCellValueFactory(cellData -> cellData.getValue().dobProperty());
+        columnPassword.setCellValueFactory(cellData -> cellData.getValue().passwordProperty());
+        columnGender.setCellValueFactory(cellData -> cellData.getValue().genderProperty());
+        columnRole.setCellValueFactory(cellData -> cellData.getValue().roleProperty());
     }
 }
